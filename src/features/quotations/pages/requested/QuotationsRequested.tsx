f124b9950530e5107bc8d4da2e20a2cf0cb1cdd1
Khatejang <aguarinmarjorie@gmail.com>
2026-04-28 14:37:12 +0800

    : bug - fix accept function

--- src/features/quotations/pages/requested/QuotationsRequested.tsx
+++ src/features/quotations/pages/requested/QuotationsRequested.tsx
@@ -286,7 +286,7 @@ export function QuotationsRequested() {
       <AcceptModal
         acceptModalOpen={acceptModalOpen}
         setAcceptModalOpen={setAcceptModalOpen}
-        onConfirm={handleReassignConfirm}
+        onConfirm={handleAcceptConfirm}
         isSubmitting={acceptQuotationMutation.isPending}
       />
 
